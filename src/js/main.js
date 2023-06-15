430885f2d51e3361adc8e96a76587e13bfcd1680
rebeccaschuell <rebecca.schuell@studentfr.ch>
2023-06-15 13:17:03 +0200

    créer le sélecteur de langues

--- src/js/main.js
+++ src/js/main.js
@@ -13,3 +13,37 @@ accordionItems.forEach((item) => {
     }
   });
 });
+
+// var blueColor = document.querySelector(".change-color");
+
+// var changeBlue = function () {
+//   blueColor.classList.add("is-blue");
+// };
+
+// blueColor.addEventListener("click", changeBlue);
+
+// document.getElementById("change-color").addEventListener("click", function () {
+//   var text = document.getElementById("change-color");
+//   text.classList.toggle("color-change");
+// });
+
+var frLanguage = document.querySelector(".fr-language");
+var deLanguage = document.querySelector(".de-language");
+
+var changeWhite = function () {
+  frLanguage.classList.remove("is-blue");
+  frLanguage.classList.add("is-white");
+  deLanguage.classList.remove("is-white");
+  deLanguage.classList.add("is-blue");
+};
+
+deLanguage.addEventListener("click", changeWhite);
+
+var changeBlue = function () {
+  deLanguage.classList.remove("is-blue");
+  deLanguage.classList.add("is-white");
+  frLanguage.classList.remove("is-white");
+  frLanguage.classList.add("is-blue");
+};
+
+frLanguage.addEventListener("click", changeBlue);
